2534289add485cb8b6814f6da19f1e30ad5c7436
Aman Sharma <166890222+amansharma1916@users.noreply.github.com>
2025-08-20 12:25:28 +0530

    Update RecievePage.jsx

--- src/components/RecievePage.jsx
+++ src/components/RecievePage.jsx
@@ -1,35 +1,56 @@
-import React from 'react'
-import './RecievePage.css'
-import { useState } from 'react'
+import React, { useState } from 'react';
+import './RecievePage.css';
+
 const RecievePage = () => {
-    const [recievedData , setRecievedData] = useState('')
-    const recieveData = () => {
-        const code = document.querySelector('input').value
-        fetch(`https://tshare-backend.onrender.com/get/${code}`)
-        .then(res => res.json())
-        .then(data => {
-            setRecievedData(data.text);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    }
+  const [recievedData, setRecievedData] = useState('');
+
+  const recieveData = () => {
+    const code = document.querySelector('input').value;
+    fetch(`https://tshare-backend.onrender.com/get/${code}`)
+      .then(res => res.json())
+      .then(data => {
+        // Unescape newline characters if returned as literal "\n"
+        const unescapedText = data.text.replace(/\\n/g, '\n').replace(/\\t/g, '\t').replace(/\\\\/g, '\\');
+        setRecievedData(unescapedText);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
   return (
     <div>
       <div className="recieveMain">
         <div className="recievedData">
-          <h1>Recieved Data : </h1>
-          <div className="data">
+          <h1>Recieved Data :</h1>
+          {/* Use a div with CSS white-space or <pre> tag to preserve formatting */}
+          <pre className="data">
             {recievedData}
-          </div>
+          </pre>
         </div>
-        <input type="text" placeholder='Enter Code' /> 
-        <button className='Btn' id='copyBtn' onClick={() => navigator.clipboard.writeText(recievedData).then(() => alert('Copied to clipboard'))}>Copy</button>
-        <button className='Btn' id='recievePageBtn' onClick={recieveData}>Recieve</button>
-        <button className='Btn' id='backBtn' onClick={()=>{window.location.href = '/'}}>Back</button>
+
+        <input type="text" placeholder="Enter Code" />
+
+        <button
+          className="Btn"
+          id="copyBtn"
+          onClick={() =>
+            navigator.clipboard.writeText(recievedData).then(() => alert('Copied to clipboard'))
+          }
+        >
+          Copy
+        </button>
+
+        <button className="Btn" id="recievePageBtn" onClick={recieveData}>
+          Recieve
+        </button>
+
+        <button className="Btn" id="backBtn" onClick={() => (window.location.href = '/')}>
+          Back
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecievePage
+export default RecievePage;
