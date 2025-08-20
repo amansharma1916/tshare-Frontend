f7d9d2461ac7f33a0a501f5f7833084b4e81c3cd
Aman Sharma <166890222+amansharma1916@users.noreply.github.com>
2025-08-20 12:24:53 +0530

    Update SharePage.jsx

--- src/components/SharePage.jsx
+++ src/components/SharePage.jsx
@@ -1,46 +1,52 @@
-import React from 'react'
-import './SharePage.css'
-import { useState } from 'react'
-
-
-
-
+import React, { useState } from 'react';
+import './SharePage.css';
 
 const SharePage = () => {
-  const [code , setCode] = useState('')
+  const [code, setCode] = useState('');
+
   const saveTextDb = () => {
-      const text = document.getElementById('sharePageInput').value
-      fetch('https://tshare-backend.onrender.com/save', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({text})
-      })
+    const text = document.getElementById('sharePageInput').value;
+    fetch('https://tshare-backend.onrender.com/save', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text }),
+    })
       .then(res => res.json())
       .then(data => {
-          setCode(data.id);
-          document.getElementById('sharePageInput').value = '';
-          alert("Text Shared Successfully");
+        setCode(data.id);
+        document.getElementById('sharePageInput').value = '';
+        alert('Text Shared Successfully');
       })
       .catch(error => {
-          console.error('Error:', error);
+        console.error('Error:', error);
       });
-  }
+  };
+
   return (
     <div>
       <div className="shareMain">
         <div className="textBox">
-            <div className="code">
-              <h1>Your Code : {code}</h1>
-            </div>
-            <input type="text" className='input' id='sharePageInput' placeholder='Input Text'/>
-            <button className='Btn' id='sharePageBtn' onClick={saveTextDb}>Share</button>
-            <button className='Btn' id='backBtn' onClick={()=>{window.location.href = '/'}}>Back</button>
+          <div className="code">
+            <h1>Your Code : {code}</h1>
+          </div>
+          <textarea
+            className="input"
+            id="sharePageInput"
+            placeholder="Input Text"
+            rows={10}
+          />
+          <button className="Btn" id="sharePageBtn" onClick={saveTextDb}>
+            Share
+          </button>
+          <button className="Btn" id="backBtn" onClick={() => { window.location.href = '/'; }}>
+            Back
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SharePage
+export default SharePage;
